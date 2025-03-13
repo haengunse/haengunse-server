e255ee742faa63700b1472d84d524668594ac1a4
giyomdabin <dabinjang01@gmail.com>
2025-03-13 16:09:30 +0900

    first commit

---
+++ src/main/java/fortune/haengunseserver/HaengunseServerApplication.java
@@ -0,0 +1,13 @@
+package fortune.haengunseserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaengunseServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaengunseServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fortune/haengunseserver/HaengunseServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fortune.haengunseserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaengunseServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
