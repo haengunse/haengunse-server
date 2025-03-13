7bf3b3e4e5e6bcaa84a0823b6bb184f9956b60cf
giyomdabin <dabinjang01@gmail.com>
2025-03-13 16:34:02 +0900

    feat: SwaggerConfig 작성

---
+++ src/main/java/fortune/haengunseserver/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package fortune.haengunseserver.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("행운세 API 명세서")
+                .description("행운세 API 명세서입니다.")
+                .version("v1");
+    }
+}
