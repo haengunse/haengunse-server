f34864fe843c47dffbb7e226f07639fbd07ab535
giyomdabin <dabinjang01@gmail.com>
2025-03-18 22:16:37 +0900

    feat: 천간-지지 데이터 추가

---
+++ src/main/java/fortune/haengunseserver/domain/lucky/enums/Jiji.java
@@ -0,0 +1,38 @@
+package fortune.haengunseserver.domain.lucky.enums;
+
+public enum Jiji {
+
+    자("자", "子", "수", "양"),
+    축("축", "丑", "토", "음"),
+    인("인", "寅", "목", "양"),
+    묘("묘", "卯", "목", "음"),
+    진("진", "辰", "토", "양"),
+    사("사", "巳", "화", "음"),
+    오("오", "午", "화", "양"),
+    미("미", "未", "토", "음"),
+    신("신", "申", "금", "양"),
+    유("유", "酉", "금", "음"),
+    술("술", "戌", "토", "양"),
+    해("해", "亥", "수", "음");
+
+    private final String name;
+    private final String hanja;
+    private final String element;
+    private final String yinYang;
+
+    Jiji(String name, String hanja, String element, String yinYang) {
+        this.name = name;
+        this.hanja = hanja;
+        this.element = element;
+        this.yinYang = yinYang;
+    }
+
+    public String getName() { return name; }
+    public String getHanja() { return hanja; }
+    public String getElement() { return element; }
+    public String getYinYang() { return yinYang; }
+
+    public static Jiji fromIndex(int index) {
+        return values()[index % 12];
+    }
+}
---
+++ src/main/java/fortune/haengunseserver/domain/lucky/enums/Tenkan.java
@@ -0,0 +1,36 @@
+package fortune.haengunseserver.domain.lucky.enums;
+
+public enum Tenkan {
+
+    갑("갑", "甲", "목", "양"),
+    을("을", "乙", "목", "음"),
+    병("병", "丙", "화", "양"),
+    정("정", "丁", "화", "음"),
+    무("무", "戊", "토", "양"),
+    기("기", "己", "토", "음"),
+    경("경", "庚", "금", "양"),
+    신("신", "辛", "금", "음"),
+    임("임", "壬", "수", "양"),
+    계("계", "癸", "수", "음");
+
+    private final String name;
+    private final String hanja;
+    private final String element;
+    private final String yinYang;
+
+    Tenkan(String name, String hanja, String element, String yinYang) {
+        this.name = name;
+        this.hanja = hanja;
+        this.element = element;
+        this.yinYang = yinYang;
+    }
+
+    public String getName() { return name; }
+    public String getHanja() { return hanja; }
+    public String getElement() { return element; }
+    public String getYinYang() { return yinYang; }
+
+    public static Tenkan fromIndex(int index) {
+        return values()[index % 10];
+    }
+}
