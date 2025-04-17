b1fb5dd6dc36cb5f812df84ad88e898111fc364a
giyomdabin <dabinjang01@gmail.com>
2025-04-17 22:07:11 +0900

    fix: 오늘의 운세 및 꿈 해석 요청 dto에 유효성 검사 코드 추가

--- src/main/java/fortune/haengunseserver/domain/fortune/dto/request/dream/DreamRequest.java
+++ src/main/java/fortune/haengunseserver/domain/fortune/dto/request/dream/DreamRequest.java
@@ -1,6 +1,7 @@
 package fortune.haengunseserver.domain.fortune.dto.request.dream;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ import java.util.List;
 @Schema(description = "해몽 요청")
 public class DreamRequest {
 
+    @NotBlank(message = "사용자의 꿈 내용을 보내주세요.")
     @Schema(description = "사용자의 질문", example = "오늘 돼지꿈 꿨는데 무슨 뜻이야?")
     private List<String> question;
 }
