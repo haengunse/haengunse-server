d16e8144d1bb880f4872c264f9fcbad4411caa5d
giyomdabin <dabinjang01@gmail.com>
2025-04-17 22:20:02 +0900

    refactor: 중복 코드 제거

--- src/main/java/fortune/haengunseserver/domain/fortune/service/dream/DreamService.java
+++ src/main/java/fortune/haengunseserver/domain/fortune/service/dream/DreamService.java
@@ -1,8 +1,6 @@
 package fortune.haengunseserver.domain.fortune.service.dream;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import fortune.haengunseserver.domain.fortune.dto.response.dream.DreamResponse;
-import fortune.haengunseserver.domain.fortune.dto.response.todayfortune.TodayFortuneResponse;
 import fortune.haengunseserver.global.gpt.service.FortuneRequestService;
 import fortune.haengunseserver.global.gpt.utils.ChatResponseParser;
 import org.springframework.ai.chat.model.ChatResponse;
@@ -23,28 +21,27 @@ public class DreamService extends FortuneRequestService<List<String>, DreamRespo
     @Override
     public Prompt generatePrompt(List<String> questions) {
 
-        // 질문이 비어 있는 경우 / 질문 개수가 초과한 경우 -> 예외처리
+        // 질문 개수가 초과한 경우 -> 예외처리
 
         StringBuilder contentBuilder = new StringBuilder();
         contentBuilder.append("다음을 보고 꿈을 해석해주세요:\n\n");
 
-        if (questions.size() == 1) {
-            contentBuilder.append(String.format("%s\n\n", questions.get(0)));
-        } else {
-            contentBuilder.append(String.format("기존 꿈:\n%s", questions.get(0)));
-            if (questions.size() == 2) {
-                contentBuilder.append(String.format("\n\n추가 꿈:\n%s\n\n", questions.get(1)));
-            } else if(questions.size() == 3) {
-                contentBuilder.append(String.format(", \n%s\n\n 추가 꿈 :\n%s\n\n", questions.get(1), questions.get(2)));
+        for (int i = 0; i < questions.size(); i++) {
+            if (i == 0) {
+                contentBuilder.append("기존 꿈:\n");
+            } else {
+                contentBuilder.append("추가 꿈:\n");
             }
+            contentBuilder.append(questions.get(i)).append("\n\n");
         }
+
 
         contentBuilder.append("""
         [답변 규칙]
         - 존댓말로 답변할 것
         - 종합해석만 대답할 것 (너무 길지 않게 부탁해요)
         - 모두 하나의 꿈으로 생각하되, 추가 꿈에 대한 답변을 해주세요.
-        - "interpretation" : ".." 형식으로 답해주세요.
+        - "interpretation" : ".." 의 json 형식으로 답해주세요.
         """);
 
         return new Prompt(contentBuilder.toString());
